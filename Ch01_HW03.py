2d1b3157cc76c359d21b5ea3b4f759998eef53ce
Amir Madankan <amir.madankan.1618@gmail.com>
2023-03-31 16:36:58 +0430

    input and output loaded and matrix created

--- Ch01_HW03.py
+++ Ch01_HW03.py
@@ -6,8 +6,10 @@ ws = load_workbook("files/Ch01_HW03_linear_regression_data.xlsx")["Sheet1"]
 
 n = ws.max_row + 1
 x = []
-
+y = []
 for i in range(2, n):
     x.append([ws[f"A{i}"].value, ws[f"B{i}"].value, ws[f"C{i}"].value])
+    y.append([ws[f"D{i}"].value])
 
 x = np.array(x).reshape(-1, 3)
+y = np.array(y).reshape(-1, 1)
