8767e8611df09118dc86cc129a9f08192c2371a6
Amir Madankan <amir.madankan.1618@gmail.com>
2023-03-31 16:34:17 +0430

    input loaded

--- Ch01_HW03.py
+++ Ch01_HW03.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from openpyxl import load_workbook
+
+ws = load_workbook("files/Ch01_HW03_linear_regression_data.xlsx")["Sheet1"]
+
+n = ws.max_row + 1
+x = []
+
+for i in range(2, n):
+    x.append([ws[f"A{i}"].value, ws[f"B{i}"].value, ws[f"C{i}"].value])
+
+x = np.array(x).reshape(-1, 3)
